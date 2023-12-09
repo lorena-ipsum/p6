4fd152632b760c3dda9b1bc1a1b74b1763125e9a
lorena-ipsum <lorenanbo@gmail.com>
2023-12-09 13:16:53 +0100

    adicionando estilos

--- wp-content/themes/p6-planty/footer.php
+++ wp-content/themes/p6-planty/footer.php
@@ -1,10 +1,11 @@
 </main>
 <?php get_sidebar(); ?>
 
+<div class=footerfoutu>
 <footer id="footer" role="contentinfo">
-   <a href="http://https://www.tibia.com/abouttibia/"> Mention legales </a>
+   <a href="http://https://www.tibia.com/abouttibia/"> Mention Legales </a>
 </footer>
-
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>
--- wp-content/themes/p6-planty/header.php
+++ wp-content/themes/p6-planty/header.php
@@ -1,26 +1,30 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width" />
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<div id="branding">
-<div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-<?php
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
-echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
-?>
-</div>
-<nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
-<div id="search"><?php get_search_form(); ?></div>
-</nav>
+    <?php wp_body_open(); ?>
+    <div id="wrapper" class="hfeed">
+    <header id="header" role="banner">
+    <div id="branding">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" id="logo">
+            <img src="/Applications/MAMP/htdocs/Planty/wp-content/themes/p6-planty/assets/img/Logo.jpg" alt="Logo do Site">
+        </a>
+    </div>
+    <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+        <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container_class' => 'menu-container' ) ); ?>
+    </nav>
 </header>
-<div id="container">
-<main id="content" role="main">
+
+        <div id="container">
+            <main id="content" role="main">
+                <!-- O conteúdo da página vai aqui -->
+            </main>
+        </div>
+    </div>
+    <?php wp_footer(); ?>
+</body>
+</html>
